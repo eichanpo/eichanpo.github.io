c18c73a9f512af987eb89038745df39adbfaa56e
eichanpo <eichanpo412@gmail.com>
2023-01-12 22:30:40 +0800

    modified

---
+++ bootstrap/hotel/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+            breakpoint: 778,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
